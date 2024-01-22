c2e7d0c9da3d3be560822901c31e50d7ed9a763c
Hierro Blado <79893038+hlink27@users.noreply.github.com>
2024-01-22 05:38:55 -0300

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
-const database = 'facilita'
-const username = 'anon'
-const password = '521478'
+const database = ''
+const username = ''
+const password = ''
 const sequelize = new Sequelize(database, username, password, {
     host: 'localhost',
     dialect: 'postgres',
